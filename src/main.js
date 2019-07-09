0ddf5d029c60dde87dfd998e1bf729a1ef777e4a
mushiwei <mushiweiit@aliyun.com>
2019-07-09 16:19:45 +0800

    'rem配置适配解决方案'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+/**
+ * 安装 npm i -S amfe-flexible
+ * 这个第三方包引入即可,它会自动根据页面宽度设置网页基准字体大小
+ * 安装 npm install postcss-pxtorem --save-dev
+ * 用于将单位转化为 rem
+ */
+import 'amfe-flexible'
 import {
   Button,
   Tabbar,
