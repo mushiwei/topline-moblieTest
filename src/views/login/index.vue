530a6a34fdb93bf8319b568f2f79b3140e16294f
mushiwei <mushiweiit@aliyun.com>
2019-07-09 21:20:45 +0800

    登录表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -11,8 +11,11 @@
           clearable
           label="手机号"
           placeholder="请输入手机号"
+          :error-message="errors.mobile"
         />
-        <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
+        <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码"
+        :error-message="errors.code"
+        required />
       </van-cell-group>
       <!-- 底部 -->
       <div class="login-btn-box">
@@ -35,12 +38,34 @@ export default {
         mobile: '18801185985',
         code: '123456'
       },
-      loginLoading: false // 控制登录按钮的loading状态
+      loginLoading: false, // 控制登录按钮的loading 状态
+      errors: {
+        mebile: '',
+        code: ''
+      }
     }
   },
   methods: {
     async handLogin () {
       try {
+        // 发送请求之前,校验表单数据,校验通过,才进行登录
+        const { mobile, code } = this.user
+        const errors = this.errors
+
+        if (mobile.length) {
+          errors.mobile = ''
+        } else {
+          errors.mobile = '手机号不能为空'
+          return
+        }
+        if (code.length) {
+          errors.code = ''
+        } else {
+          errors.code = '手机号不能为空'
+          return
+        }
+        // 表单验证通过,发送请求,loading 加载
+        this.loginLoading = true
         const data = await login(this.user)
         this.$store.commit('setUser', data)
         /**
