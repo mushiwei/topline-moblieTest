326dc9673c1a97e56639f3742d691c8ad0a6598f
mushiwei <mushiweiit@aliyun.com>
2019-07-11 17:50:49 +0800

    登录表单验证

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate, { Validator } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN'
+/**
+ * 这个第三方包引入即可,它会自动根据页面宽度设置网页基准字体大小
+ */
 import 'amfe-flexible'
 import Vant from 'vant'
 import 'vant/lib/index.css'
-
+Vue.use(VeeValidate, {
+  events: '' // 禁用默认事件验证
+  // events: 'change'
+})
+Validator.localize('zh_CN', zhCN)
 Vue.use(Vant)
 /**
  * 现在我们简单粗暴全引进来,方便使用
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,28 +3,39 @@
     <!-- 头部 -->
     <van-nav-bar title="登录" />
     <!-- 表单 -->
-    <from>
+    <form>
       <van-cell-group>
         <van-field
-          v-model="user.mobile"
+          v-validare=" 'required'"
+          name="mobile"
           required
           clearable
           label="手机号"
           placeholder="请输入手机号"
-          :error-message="errors.mobile"
+          :error-message="errors.firse('mobile')"
         />
-        <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码"
-        :error-message="errors.code"
-        required />
+        <van-field
+          v-model="user.code"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          name="code"
+          :error-message="errors.first('code')"
+          v-validate="required"
+          required
+        />
       </van-cell-group>
       <!-- 底部 -->
       <div class="login-btn-box">
-        <van-button type="info" class="login-btn"
-        :loading="loginLoading"
-        loading-text="登录中..."
-        @click.prevent="handLogin">登录</van-button>
+        <van-button
+          type="info"
+          class="login-btn"
+          :loading="loginLoading"
+          loading-text="登录中..."
+          @click.prevent="handleLogin"
+        >登录</van-button>
       </div>
-    </from>
+    </form>
   </div>
 </template>
 
@@ -34,62 +45,79 @@ export default {
   name: 'LoginIndex',
   data () {
     return {
-      user: { // 提交登录的表单数据
+      user: {
+        // 提交登录的表单数据
         mobile: '18801185985',
         code: '123456'
       },
       loginLoading: false, // 控制登录按钮的loading 状态
-      errors: {
+      myErrors: {
         mebile: '',
         code: ''
       }
     }
   },
+  created () {
+    this.configFormErrorMessages()
+  },
   methods: {
-    async handLogin () {
+    async handleLogin () {
       try {
-        // 发送请求之前,校验表单数据,校验通过,才进行登录
-        const { mobile, code } = this.user
-        const errors = this.errors
+        // 调用 JavaScript 触发验证
+        const valid = await this.$validator.validate()
 
-        if (mobile.length) {
-          errors.mobile = ''
-        } else {
-          errors.mobile = '手机号不能为空'
+        // 如果校验失败，则停止后续代码执行
+        if (!valid) {
           return
         }
-        if (code.length) {
-          errors.code = ''
-        } else {
-          errors.code = '手机号不能为空'
-          return
-        }
-        // 表单验证通过,发送请求,loading 加载
+
+        // 表单验证通过，发送请求，loading 加载
         this.loginLoading = true
+
         const data = await login(this.user)
+
         this.$store.commit('setUser', data)
+
         /**
-         * 這裡先簡單粗暴跳轉的首頁
-         * 真實的業務處理成跳轉到之前過來的頁面
+         * 这里先简单粗暴的跳转到首页
+         * 真实的业务要处理成跳转到之前过来的的页面
          */
-        // this.$router.push({
-        //   name: 'home'
-        // })
+        this.$router.push({
+          name: 'home'
+        })
       } catch (err) {
-        console.log(err)
-        console.log('登录失败')
+        this.$toast.fail('登录失败')
       }
       this.loginLoading = false
+    },
+    configFormErrorMessages () {
+      const dict = {
+        custom: {
+          mobile: {
+            required: '手机号不能为空'
+          },
+          code: {
+            required: '密码不能为空'
+          }
+        }
+      }
+
+      // 如果需要错误消息提示全局生效
+      // Validator.localize('en', dict);
+
+      // 组件中这也注册生效
+      // or use the instance method
+      this.$validator.localize('zh_CN', dict)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
- .login-btn-box {
-   padding: 20px;
-   .login-btn {
-     width: 100%;
-   }
- }
+.login-btn-box {
+  padding: 20px;
+  .login-btn {
+    width: 100%;
+  }
+}
 </style>
